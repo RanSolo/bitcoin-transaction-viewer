7629b9acf37b1d1a6f98ebd8d0b46fc5aa84461e
RanSolo <ransolo@me.com>
2024-05-29 13:01:27 -0500

    fetch-transactions-from-transactions/[address] page

--- app/transactions/[address]/page.tsx
+++ app/transactions/[address]/page.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 
 interface Props { params: { address: string }}
-const TransactionsByAddress = ({ params: { address } }: Props) => (
-    <div>
+interface Transaction {
+    txid: string
+    date: string
+    amount: number
+    status: {
+        confirmed: boolean
+    }
+}
+const TransactionsByAddress = async ({ params: { address } }: Props) => {
+    const response = await fetch(`http:localhost:3000/api/transactions/${address}`)
+    const transactions: Transaction[] = await response.json();
+    
+    return <div>
         <h1>TransactionsByAddress {address}</h1>
-    </div>
-)
+        <ul>
+            {transactions.map(transaction => 
+                <li className="p-10" key={transaction.txid}>txid: {transaction.txid}</li>
+            )}
+        </ul>
+    </div>;
+}
 
 export default TransactionsByAddress
